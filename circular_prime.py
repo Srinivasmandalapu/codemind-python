eafd7ef79a74ca9978b723562637ea65800944e1
Srinivasmandalapu <97425298+Srinivasmandalapu@users.noreply.github.com>
2022-10-15 00:38:10 +0530

    n=int(input()) p=0 def is_prime(s):     if(s==1):         return 0     for i in range(2,int(s**0.5)+1):         if(s%i==0):             return 0     else:         return 1 def is_rev(s):     rev=0     d=0     while(s!=0):         d=s%10         s=s//10         rev=rev*10+d     return rev if(is_prime(n)):     p=is_rev(n)     if(is_prime(p)):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,24 +1,27 @@
 n=int(input())
-c=0
-s=0
-for i in range(2,(n//2)+1):
-    if(n%i==0):
-        break
-else:
-    c=c+1
-while(n>0):
-    r=n%10
-    s=s*10+r
-    n=n//10
-for i in range(2,(s//2)+1):
-    if(s%i==0):
-        break
-else:
-    c=c+1
-if(c==2):
-    print("circular prime")
-elif(c==1):
-    print("prime but not a circular prime")
+p=0
+def is_prime(s):
+    if(s==1):
+        return 0
+    for i in range(2,int(s**0.5)+1):
+        if(s%i==0):
+            return 0
+    else:
+        return 1
+def is_rev(s):
+    rev=0
+    d=0
+    while(s!=0):
+        d=s%10
+        s=s//10
+        rev=rev*10+d
+    return rev
+if(is_prime(n)):
+    p=is_rev(n)
+    if(is_prime(p)):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
     print("not prime")
     
