c53d08f3c2a40ff774eb61f4971cd5ee0169a565
Srinivasmandalapu <97425298+Srinivasmandalapu@users.noreply.github.com>
2022-10-15 23:29:06 +0530

    n,m=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) c=0 p=[] for i in l1:     if(i in l2 and i not in p):         p.append(i)         c+=1 print(c)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,24 +1,10 @@
-a,b=map(int,input().split())
-arr=list(map(int,input().split()))
-arr2=list(map(int,input().split()))
-x=0
-for i in range(a):
-    for j in range(a):
-        if i!=j:
-            if arr[i]==arr[j]:
-                if arr[i]!=-1000:
-                    arr[j]=-1000
-for i in range(b):
-    for j in range(b):
-        if i!=j:
-            if arr2[i]==arr2[j] and arr2[i]!=-100:
-                arr2[j]=-100
-for i in arr:
-    if i!=-1:
-        if i in arr2:
-            x+=1
-for i in arr2:
-    if i!=-1:
-        if i in arr:
-            x+=1
-print(x//2)
+n,m=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+c=0
+p=[]
+for i in l1:
+    if(i in l2 and i not in p):
+        p.append(i)
+        c+=1
+print(c)
